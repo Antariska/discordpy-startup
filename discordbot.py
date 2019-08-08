c0e1b7d9e239cef6cec5140bb1a06294c745ee2a
Antariska <53042031+Antariska@users.noreply.github.com>
2019-08-08 13:28:53 +0900

    v4.3.1

--- discordbot.py
+++ discordbot.py
@@ -21,6 +21,8 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+
+    await client.change_presence(activity=discord.Game(name='/boshu'))
             
 
 # on command
